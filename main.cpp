263a588ef8937d7e0deb4f4592f3338e6d02726e
Ashraful-notfOuND <113161861+Ashraful-notfOuND@users.noreply.github.com>
2023-11-05 16:55:30 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include"defs.hpp"
+#include"init.hpp"
 
+int main(int argc, char *argv[])
+{
+	App app;
+	memset(&app, 0, sizeof(App));
+
+	initSDL();
+
+	while (1)
+	{
+		prepareScene();
+
+		doInput();
+
+		presentScene();
+
+		SDL_Delay(16);
+	}
+
+	return 0;
+}
