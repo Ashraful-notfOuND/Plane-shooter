d51d6197d9b50edcd32894c3d3b5d09d1dcf272b
Ashraful-notfOuND <113161861+Ashraful-notfOuND@users.noreply.github.com>
2023-11-10 01:53:16 +0600

    Update defs.hpp

--- defs.hpp
+++ defs.hpp
@@ -1,12 +1,30 @@
-#include<iostream>
+#ifndef _DEFS_H_
+#define _DEFS_H_
+
+#include<bits/stdc++.h>
 #include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
 
-#define SCREEN_WIDTH   1280
-#define SCREEN_HEIGHT  720
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
-typedef struct {
+struct App{
 	SDL_Renderer *renderer;
 	SDL_Window *window;
-} App;
+	int up;
+	int down;
+	int left;
+	int right;
+	int fire;
+}app;
 
+struct Entity{
+	int x;
+	int y;
+	int dx;
+	int dy;
+	int health;
+	SDL_Texture *texture;
+} player,bullet;
 
+#endif
