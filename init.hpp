c420261148008fccd6228f8e8d823a69a325a8ad
Ashraful-notfOuND <113161861+Ashraful-notfOuND@users.noreply.github.com>
2023-11-10 01:53:44 +0600

    Update init.hpp

--- init.hpp
+++ init.hpp
@@ -1,7 +1,15 @@
+#ifndef _INIT_H_
+#define _INIT_H_
+
 #include"defs.hpp"
+#include"init.cpp"
+
 
 void initSDL(void);
 void doInput(void);
 void prepareScene(void);
 void presentScene(void);
+void doKeyDown(void);
+void doKeyUp(void);
 
+#endif
