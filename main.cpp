2f443399a5c3edbe79251dfb869c4066753670af
Ashraful-notfOuND <113161861+Ashraful-notfOuND@users.noreply.github.com>
2023-11-10 01:52:13 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include"defs.hpp"
 #include"init.hpp"
 
-int main(int argc, char *argv[])
+int main()
 {
-	App app;
 	memset(&app, 0, sizeof(App));
+	memset(&player, 0, sizeof(Entity));
+	memset(&bullet, 0, sizeof(Entity));
 
 	initSDL();
+
+	// atexit(cleanup);
+
+	player.texture = loadTexture("player.png");
+	player.x = 100;
+	player.y = 100;
+
+	bullet.texture = loadTexture("playerBullet.png");
 
 	while (1)
 	{
@@ -14,9 +23,56 @@ int main(int argc, char *argv[])
 
 		doInput();
 
+		player.x += player.dx;
+		player.y += player.dy;
+
+		if (app.up)
+		{
+			player.y -= 4;
+		}
+
+		if (app.down)
+		{
+			player.y += 4;
+		}
+
+		if (app.left)
+		{
+			player.x -= 4;
+		}
+
+		if (app.right)
+		{
+			player.x += 4;
+		}
+
+		if (app.fire && bullet.health == 0)
+		{
+			bullet.x = player.x;
+			bullet.y = player.y;
+			bullet.dx = 32;
+			bullet.dy = 0;
+			bullet.health = 1;
+		}
+
+		bullet.x += bullet.dx;
+		bullet.y += bullet.dy;
+
+		if (bullet.x > SCREEN_WIDTH)
+		{
+			bullet.health = 0;
+		}
+
+		blit(player.texture, player.x, player.y);
+
+		if (bullet.health > 0)
+		{
+			blit(bullet.texture, bullet.x, bullet.y);
+		}
+
 		presentScene();
 
-		SDL_Delay(16);
+		SDL_Delay(8);
 	}
 
 	return 0;
